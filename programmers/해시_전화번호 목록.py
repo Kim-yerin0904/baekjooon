2b266305c73a744b030f915b9dbceafc53393a34
Kim-yerin0904 <77713307+Kim-yerin0904@users.noreply.github.com>
2022-01-04 20:33:41 +0900

    Update 해시_전화번호 목록.py

--- programmers/해시_전화번호 목록.py
+++ programmers/해시_전화번호 목록.py
@@ -1,1 +1,13 @@
-
+def solution(phone_book):
+    phone_book.sort()
+    answer = True
+    pre = phone_book[0]
+    for i in range(1,len(phone_book)):
+        compare = phone_book[i][:len(pre)]
+        if pre == compare:
+            answer = False
+            break
+        pre = phone_book[i]
+        if answer == False:
+            break
+    return answer
